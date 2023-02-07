d706887b0a54ab4e021de72c8a81df2395003513
mountisome <1981738965@qq.com>
2023-02-07 21:55:08 +0800

    完成服务请求页面

--- pages/user/request/request.vue
+++ pages/user/request/request.vue
@@ -1,29 +1,19 @@
 <template>
 	<view>
 		<uni-section class="mb-10" title="已解决" type="line">
-			<uni-collapse  accordion v-model="accordionVal">
-				<uni-collapse-item title="请求1">
+			<uni-collapse  accordion v-model="accordionVal" v-for="(request, index) in solvedRequest">
+				<uni-collapse-item :title="'请求'+(index+1)">
 					<uni-card title="请求:" :thumbnail="image1">
-						<text>第一个请求</text>
+						<text>{{request.requestContent}}</text>
+						<view>
+							<uni-dateformat :date="request.requestTime"></uni-dateformat>
+						</view>
 					</uni-card>
 					<uni-card title="回复:" :thumbnail="image2">
-						<text>第一个回复</text>
-					</uni-card>
-				</uni-collapse-item>
-				<uni-collapse-item title="请求2">
-					<uni-card title="请求:" :thumbnail="image1">
-						<text>第二个请求</text>
-					</uni-card>
-					<uni-card title="回复:" :thumbnail="image2">
-						<text>第二个回复</text>
-					</uni-card>
-				</uni-collapse-item>
-				<uni-collapse-item title="请求3">
-					<uni-card title="请求:" :thumbnail="image1">
-						<text>第三个个请求</text>
-					</uni-card>
-					<uni-card title="回复:" :thumbnail="image2">
-						<text>第三个回复</text>
+						<text>{{request.responseContent}}</text>
+						<view>
+							<uni-dateformat :date="request.responseTime"></uni-dateformat>
+						</view>
 					</uni-card>
 				</uni-collapse-item>
 			</uni-collapse>
@@ -34,21 +24,22 @@
 			<template v-slot:decoration>
 			  <view class="decoration"></view>
 			</template>
-			<uni-collapse>
-				<uni-collapse-item title="未解决请求1">
+			<uni-collapse v-for="(request, index) in unsolvedRequest">
+				<uni-collapse-item :title="'请求'+(index+1)">
 					<uni-card title="请求:" :thumbnail="image1">
-						<text>第一个请求</text>
-					</uni-card>
-					<uni-card title="回复:" :thumbnail="image2">
-						<text>第一个回复，我希望不要有bug</text>
+						<text>{{request.requestContent}}</text>
+						<view>
+							<uni-dateformat :date="request.requestTime"></uni-dateformat>
+						</view>
 					</uni-card>
 				</uni-collapse-item>
 			</uni-collapse>
 		</uni-section>
 	</view>
 	<view>
-		<uni-section class="mb-10" title="提出请求">
-			
+		<uni-section class="mb-10" title="提出请求" padding>
+			<uni-easyinput type="textarea" v-model="value" placeholder="请输入内容"></uni-easyinput>
+			<button type="primary" plain="true" size="mini" @click="submitRequest()">提交</button>
 		</uni-section>
 	</view>
 </template>
@@ -59,13 +50,68 @@
 			return {
 				accordionVal: '',
 				image1: '',
-				image2: '/static/images/user.png'
+				image2: '/static/images/user.png',
+				value: '',
+				username: '',
+				solvedRequest: [],
+				unsolvedRequest: []
 			}
 		},
 		onLoad() {
+			this.username = uni.getStorageSync('username')
 			this.image1 = uni.getStorageSync('image')
+			const db = uniCloud.database()
+			db.collection('request').get().then((res)=>{
+				let len = res.result.data.length
+				if (len > 0) {
+					let k = 0
+					let t = 0
+					for (let i = 0; i < len; i++) {
+						if (res.result.data[i].name == this.username) {
+							if (res.result.data[i].solved) {
+								this.$set(this.solvedRequest, k, {
+										requestContent: res.result.data[i].requestContent,
+										requestTime: res.result.data[i].requestTime,
+										responseContent: res.result.data[i].responseContent,
+										responseTime: res.result.data[i].responseTime	
+								})
+								k++
+							} else {
+								this.$set(this.unsolvedRequest, t, {
+									requestContent: res.result.data[i].requestContent,
+									requestTime: res.result.data[i].requestTime
+								})
+								t++
+							}
+						}
+					}
+				}
+			}).catch((err)=>{
+				console.log(err.code)
+				console.log(err.message)
+			})
 		},
 		methods: {
+			submitRequest() {
+				let timestamp = new Date().getTime()
+				const db = uniCloud.database()
+				db.collection('request').add({
+					name: this.username,
+					requestContent: this.value,
+					requestTime: timestamp,
+					solved: false,
+					responseContent: "",
+					responseTime: 0
+				}).then((res)=>{
+					uni.showToast({
+						title: '请求提交成功',
+						icon: 'success'
+					})
+				}).catch((err)=>{
+					console.log(err.code)
+					console.log(err.message)
+				})
+			}
 		}
 	}
 </script>
